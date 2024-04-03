2c014e200f55f681db2a9072078aa8a51af2040e
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-04-02 21:50:07 -0400

    got landing page done moving to login page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -16,13 +16,12 @@ function LandingPage() {
   return (
     <div className="container">
       <h2>{heading}</h2>
-
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
             This is Xavier's Treasure Map, it is a super duper secret map with a
-            list places that the great explore Xavier has visited or heard about
-            in his travels through out the mitten.
+            list of places that the great explorer Xavier del pajaro has visited
+            or heard about in his travels through-out the 'mitten'.
           </p>
           <p>
             He would love to share this great wealth of information with you
@@ -42,6 +41,19 @@ function LandingPage() {
             </button>
           </center>
         </div>
+      </div>
+      <div className="grid-col_12">
+        Photo by{' '}
+        <a
+          href="https://unsplash.com/@anniespratt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+          color="aquamarine"
+        >
+          Annie Spratt
+        </a>{' '}
+        on{' '}
+        <a href="https://unsplash.com/photos/child-looking-at-map-kZO9xqmO_TA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+          Unsplash
+        </a>
       </div>
     </div>
   );
