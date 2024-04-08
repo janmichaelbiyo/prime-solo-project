651baa68803b50896493ed68d628087b807da5a9
Jan Michael Biyo <janmichael.biyo@gmail.com>
2024-04-08 00:27:57 -0400

    got the reviews page finished now its just the inclusive page left

--- src/components/ReviewsPage/ReviewsPage.jsx
+++ src/components/ReviewsPage/ReviewsPage.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './ReviewsPage.css';
+import { Box, Grid } from '@mui/material';
+import Button from '@mui/material/Button';
+import AttractionsIcon from '@mui/icons-material/Attractions';
+import DoneOutlineIcon from '@mui/icons-material/DoneOutline';
 
 function ReviewsPage() {
   const history = useHistory();
@@ -32,37 +36,78 @@ function ReviewsPage() {
   };
 
   return (
-    <div className="reviewMain">
-      <h1>{parksnplay.title}</h1>
-      {review.map((review) => {
-        return <p>{review.review_analysis}</p>;
-      })}
-      <form>
-        <label> Review: </label> <br></br>
-        <textarea
-          rows="30"
-          cols="150"
-          value={newReview.review_analysis}
-          onChange={(event) =>
-            setNewReview({
-              ...newReview,
-              review_analysis: event.target.value,
-              location_id: parksnplayid,
-            })
-          }
-        ></textarea>
-      </form>
-      <button onClick={handleAddReview}>Submit</button>
-      <button onClick={handleParksNPlayPage}>Parks & Playground</button>
-      Photo by{' '}
-      <a href="https://unsplash.com/@towfiqu999999?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-        Towfiqu barbhuiya
-      </a>{' '}
-      on{' '}
-      <a href="https://unsplash.com/photos/a-row-of-yellow-stars-sitting-on-top-of-a-blue-and-pink-surface-0ZUoBtLw3y4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-        Unsplash
-      </a>
-    </div>
+    <Box className="reviewMain">
+      <Grid container spacing={2} alignItems={'center'}>
+        <Grid xs={12} item>
+          <h1 className="textCenter">{parksnplay.title}</h1>
+        </Grid>
+        <Grid xs={12} item>
+          <Box className="listR">
+            {review.map((review) => {
+              return (
+                <ul>
+                  <li>{review.review_analysis}</li>
+                </ul>
+              );
+            })}
+          </Box>
+        </Grid>
+        <Grid xs={12} item>
+          <Box className="form">
+            <form>
+              <label> Review: </label> <br></br>
+              <textarea
+                rows="30"
+                cols="115"
+                value={newReview.review_analysis}
+                onChange={(event) =>
+                  setNewReview({
+                    ...newReview,
+                    review_analysis: event.target.value,
+                    location_id: parksnplayid,
+                  })
+                }
+              ></textarea>
+            </form>
+            <Button
+              onClick={handleAddReview}
+              variant="contained"
+              size="large"
+              sx={{ backgroundColor: 'purple' }}
+              startIcon={<DoneOutlineIcon />}
+            >
+              Submit
+            </Button>
+          </Box>
+        </Grid>
+        <Grid xs={12} item>
+          <Box textAlign={'center'} sx={{ paddingTop: 5 }}>
+            <Button
+              onClick={handleParksNPlayPage}
+              variant="contained"
+              size="large"
+              sx={{ backgroundColor: 'purple' }}
+              startIcon={<AttractionsIcon />}
+            >
+              Parks & Playground
+            </Button>
+          </Box>
+        </Grid>
+
+        <Grid xs={12} item>
+          <Box textAlign={'right'}>
+            Photo by{' '}
+            <a href="https://unsplash.com/@towfiqu999999?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+              Towfiqu barbhuiya
+            </a>{' '}
+            on{' '}
+            <a href="https://unsplash.com/photos/a-row-of-yellow-stars-sitting-on-top-of-a-blue-and-pink-surface-0ZUoBtLw3y4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+              Unsplash
+            </a>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 export default ReviewsPage;
